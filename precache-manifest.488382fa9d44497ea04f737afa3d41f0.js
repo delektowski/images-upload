c6b2671dea846df7bd18e98f927e648fbdd18ecc
Marcin Delektowski <marcin.delektowski@gmail.com>
2019-01-16 13:42:27 +0100

    Updates

---
+++ precache-manifest.488382fa9d44497ea04f737afa3d41f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "afe1592f0ac940bceea0",
+    "url": "/images-upload/static/js/runtime~main.afe1592f.js"
+  },
+  {
+    "revision": "ef5801fb665118019866",
+    "url": "/images-upload/static/js/main.ef5801fb.chunk.js"
+  },
+  {
+    "revision": "1b06a8edafd9ef4b6ead",
+    "url": "/images-upload/static/js/1.1b06a8ed.chunk.js"
+  },
+  {
+    "revision": "ef5801fb665118019866",
+    "url": "/images-upload/static/css/main.b6cad80d.chunk.css"
+  },
+  {
+    "revision": "2e45f0c9ef4d515f9040f550bed95fb1",
+    "url": "/images-upload/index.html"
+  }
+];
