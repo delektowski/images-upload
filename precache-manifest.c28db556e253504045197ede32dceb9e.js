ccbdc6549affdf6131549a7867d3d54a9e343bb5
Marcin Delektowski <marcin.delektowski@gmail.com>
2019-02-13 23:51:37 +0100

    Updates

--- precache-manifest.c28db556e253504045197ede32dceb9e.js
+++ precache-manifest.c28db556e253504045197ede32dceb9e.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/images-upload/static/js/runtime~main.afe1592f.js"
   },
   {
-    "revision": "851500e0acedc0d7eae5",
-    "url": "/images-upload/static/js/main.851500e0.chunk.js"
+    "revision": "f7cb96a01d0c60aa41cc",
+    "url": "/images-upload/static/js/main.f7cb96a0.chunk.js"
   },
   {
     "revision": "46f7e4eb26a735528fa3",
     "url": "/images-upload/static/js/1.46f7e4eb.chunk.js"
   },
   {
-    "revision": "851500e0acedc0d7eae5",
+    "revision": "f7cb96a01d0c60aa41cc",
     "url": "/images-upload/static/css/main.adce53b0.chunk.css"
   },
   {
-    "revision": "c300aa7cc55d983fbb39eb1d7a8444a7",
+    "revision": "27dfe78413de493a980b1d1de83076bc",
     "url": "/images-upload/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/images-upload/precache-manifest.03c5c672ba756dec3f534d3c25c31d21.js"
+  "/images-upload/precache-manifest.c28db556e253504045197ede32dceb9e.js"
 );
 
 workbox.clientsClaim();
